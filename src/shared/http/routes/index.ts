be458e19153f0344219189d8f4a807a421b0122f
andsu <andsu.marks.r@gmail.com>
2024-02-01 11:45:12 -0300

    add AppError

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,8 +1,10 @@
+import { AppError } from '@shared/errors/appError';
 import { Router } from 'express';
 
 const routes = Router();
 
 routes.get('/', (request, response) => {
+  throw new Error('acesso negado');
   return response.json({ message: 'Ola dev' });
 });
 
