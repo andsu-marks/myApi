327583639e63a6071dd9613acb6a7004332f14d6
andsu <andsu.marks.r@gmail.com>
2024-02-01 11:17:54 -0300

    Configure express and add .env

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,7 +1,18 @@
-export class AppServer {
-  private app: string;
+import 'dotenv/config';
+import express from 'express';
+import 'express-async-errors';
+import cors from 'cors';
 
-  constructor(info: string) {
-    this.app = info ?? 'Olá dev';
-  }
-}
+const PORT = process.env.PORT;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (request, response) => {
+  return response.json({ message: 'Ola dev' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}!`);
+});
