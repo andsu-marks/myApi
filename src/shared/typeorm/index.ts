5ae5609a809e304ff0fcb117edef1c3bfb6eee69
andsu <andsu.marks.r@gmail.com>
2024-02-21 11:43:41 -0300

    Create foreignKey roleiD

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -2,10 +2,11 @@ import { DataSource } from 'typeorm';
 import { Role } from '@roles/entities/Role';
 import { CreateRolesTable1708525259112 } from './migrations/1708525259112-CreateRolesTable';
 import { CreateUsersTable1708525420889 } from './migrations/1708525420889-CreateUsersTable';
+import { AddRoleIdToUsersTable1708525954444 } from './migrations/1708525954444-AddRoleIdToUsersTable';
 
 export const dataSource = new DataSource({
   type: 'sqlite',
   database: './db.sqlite',
   entities: [Role],
-  migrations: [CreateRolesTable1708525259112, CreateUsersTable1708525420889]
+  migrations: [CreateRolesTable1708525259112, CreateUsersTable1708525420889, AddRoleIdToUsersTable1708525954444]
 });
