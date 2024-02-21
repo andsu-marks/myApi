2bae65dcacc03e933c5598a4f07828cd2c7d606c
andsu <andsu.marks.r@gmail.com>
2024-02-21 11:31:11 -0300

    Create Users table

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -1,10 +1,11 @@
 import { DataSource } from 'typeorm';
-import { CreateRolesTable1706994245388 } from './migrations/1706994245388-CreateRolesTable';
 import { Role } from '@roles/entities/Role';
+import { CreateRolesTable1708525259112 } from './migrations/1708525259112-CreateRolesTable';
+import { CreateUsersTable1708525420889 } from './migrations/1708525420889-CreateUsersTable';
 
 export const dataSource = new DataSource({
   type: 'sqlite',
   database: './db.sqlite',
   entities: [Role],
-  migrations: [CreateRolesTable1706994245388]
+  migrations: [CreateRolesTable1708525259112, CreateUsersTable1708525420889]
 });
