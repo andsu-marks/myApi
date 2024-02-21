9176e209dcf1a8d3ef84b01cb6a17bfdca731d01
andsu <andsu.marks.r@gmail.com>
2024-02-21 11:59:30 -0300

    Configuring User entity

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -3,10 +3,11 @@ import { Role } from '@roles/entities/Role';
 import { CreateRolesTable1708525259112 } from './migrations/1708525259112-CreateRolesTable';
 import { CreateUsersTable1708525420889 } from './migrations/1708525420889-CreateUsersTable';
 import { AddRoleIdToUsersTable1708525954444 } from './migrations/1708525954444-AddRoleIdToUsersTable';
+import { User } from '@users/entities/User';
 
 export const dataSource = new DataSource({
   type: 'sqlite',
   database: './db.sqlite',
-  entities: [Role],
+  entities: [Role, User],
   migrations: [CreateRolesTable1708525259112, CreateUsersTable1708525420889, AddRoleIdToUsersTable1708525954444]
 });
