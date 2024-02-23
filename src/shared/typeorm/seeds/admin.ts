b7d1bf8bee18003e918c0373dbeb4a2488f1e0d7
andsu <andsu.marks.r@gmail.com>
2024-02-23 12:01:28 -0300

    Create seed admin user

--- src/shared/typeorm/seeds/admin.ts
+++ src/shared/typeorm/seeds/admin.ts
@@ -11,7 +11,7 @@ async function create() {
   // Create user
    const userId = uuidv4();
    const password = await hash("1234", 10);
-   await connection.query(`INSERT INTO users(id, name,email, password, "isAdmin", roleId)
+   await connection.query(`INSERT INTO users(id, name, email, password, "isAdmin", roleId)
    values('${userId}', 'admin', 'a@a.com', '${password}', true, '${roleId}')`);
    await connection.destroy()
 }
