ea22998d4cf84533788d6c68e5c2697f773ce8b0
andsu <andsu.marks.r@gmail.com>
2024-02-23 10:08:51 -0300

    Add CreateLoginController

--- src/users/container/index.ts
+++ src/users/container/index.ts
@@ -3,9 +3,11 @@ import { CreateUserController } from "@users/UseCases/createUser/CreateUserContr
 import { IUsersRepository } from "@users/repositories/IUserRepository";
 import { UsersRepository } from "@users/repositories/UsersRepository";
 import { ListUsersController } from "@users/UseCases/listUsers/ListUsersController";
+import { CreateLoginController } from "@users/UseCases/createLogin/CreateLoginController";
 
 container.registerSingleton<IUsersRepository>('UsersRepository', UsersRepository);
 
 container.registerSingleton('CreateUserController', CreateUserController);
 container.registerSingleton('ListUsersController', ListUsersController);
+container.registerSingleton('CreateLoginController', CreateLoginController);
 
--- src/users/http/users.routes.ts
+++ src/users/http/users.routes.ts
@@ -3,10 +3,12 @@ import { container } from "tsyringe";
 import { CreateUserController } from "@users/UseCases/createUser/CreateUserController";
 import { Joi, Segments, celebrate } from "celebrate";
 import { ListUsersController } from "@users/UseCases/listUsers/ListUsersController";
+import { CreateLoginController } from "@users/UseCases/createLogin/CreateLoginController";
 
 const usersRouter = Router();
 const createUserController = container.resolve(CreateUserController);
 const listUserController = container.resolve(ListUsersController);
+const createLoginController = container.resolve(CreateLoginController);
 
 usersRouter.post('/', celebrate({
   [Segments.BODY]: {
@@ -27,6 +29,15 @@ usersRouter.get('/', celebrate({
   }
 }), (request, response) => {
   return listUserController.handle(request, response);
+})
+
+usersRouter.post('/login', celebrate({
+  [Segments.BODY]: {
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+  }
+}), (request, response) => {
+  return createLoginController.handle(request, response);
 })
 
 export { usersRouter };
