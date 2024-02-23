4f61632013a29969ded28818d78ad837579d9d90
andsu <andsu.marks.r@gmail.com>
2024-02-23 11:13:09 -0300

    Refactor Middlaware isAuthenticated

--- src/shared/http/middlewares/isAuthenticated.ts
+++ src/shared/http/middlewares/isAuthenticated.ts
@@ -3,6 +3,10 @@ import { NextFunction, Request, Response } from 'express';
 import { Secret, verify } from 'jsonwebtoken';
 import authConfig from "@config/auth";
 
+type JwtPayloadProps = {
+  sub: string;
+}
+
 export const isAuthenticated = (request: Request, response: Response, next: NextFunction) => {
   const authHeader = request.headers.authorization;
   if (!authHeader) {
@@ -12,7 +16,9 @@ export const isAuthenticated = (request: Request, response: Response, next: Next
   const token = authHeader.replace("Bearer ", "");
 
   try {
-    verify(token, authConfig.jwt.secret as Secret);
+    const decodedToken = verify(token, authConfig.jwt.secret as Secret);
+    const { sub } = decodedToken as JwtPayloadProps;
+    request.user = { id: sub };
     return next();
   } catch {
     throw new AppError("Invalid authentication token", 401);
