44c3234e8dc872a8633c96828d14ef211f1ba22a
andsu <andsu.marks.r@gmail.com>
2024-02-25 02:56:07 -0300

    Create RefreshToken entity

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -5,11 +5,12 @@ import { CreateUsersTable1708525420889 } from './migrations/1708525420889-Create
 import { AddRoleIdToUsersTable1708525954444 } from './migrations/1708525954444-AddRoleIdToUsersTable';
 import { User } from '@users/entities/User';
 import { CreateRefreshTokensTable1708839181396 } from './migrations/1708839181396-CreateRefreshTokensTable';
+import { RefreshToken } from '@users/entities/RefreshToken';
 
 export const dataSource = new DataSource({
   type: 'sqlite',
   database: './db.sqlite',
-  entities: [Role, User],
+  entities: [Role, User, RefreshToken],
   migrations: [
     CreateRolesTable1708525259112,
     CreateUsersTable1708525420889,
