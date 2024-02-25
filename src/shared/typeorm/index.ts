0daa37d8c6483aa0c3b9e556bc847d3096f4e2db
andsu <andsu.marks.r@gmail.com>
2024-02-25 02:45:41 -0300

    Create migration CreateRefreshTokensTable

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -4,10 +4,16 @@ import { CreateRolesTable1708525259112 } from './migrations/1708525259112-Create
 import { CreateUsersTable1708525420889 } from './migrations/1708525420889-CreateUsersTable';
 import { AddRoleIdToUsersTable1708525954444 } from './migrations/1708525954444-AddRoleIdToUsersTable';
 import { User } from '@users/entities/User';
+import { CreateRefreshTokensTable1708839181396 } from './migrations/1708839181396-CreateRefreshTokensTable';
 
 export const dataSource = new DataSource({
   type: 'sqlite',
   database: './db.sqlite',
   entities: [Role, User],
-  migrations: [CreateRolesTable1708525259112, CreateUsersTable1708525420889, AddRoleIdToUsersTable1708525954444]
+  migrations: [
+    CreateRolesTable1708525259112,
+    CreateUsersTable1708525420889,
+    AddRoleIdToUsersTable1708525954444,
+    CreateRefreshTokensTable1708839181396
+  ]
 });
