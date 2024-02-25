5b2dc383f2c95f7bdb9d7388a0790822b7734391
andsu <andsu.marks.r@gmail.com>
2024-02-25 03:25:50 -0300

    Create RefreshToken - interface

---
+++ src/users/repositories/IRefreshTokenRepository.ts
@@ -0,0 +1,14 @@
+import { RefreshToken } from "@users/entities/RefreshToken";
+
+type CreateRefreshTokenDTO = {
+  user_id: string;
+  token: string;
+  expires: string;
+  valid: boolean;
+}
+
+export interface IRefreshTokenRepository {
+  create({expires, token, user_id, valid}: CreateRefreshTokenDTO): Promise<RefreshToken>;
+  findByToken(token: string): Promise<RefreshToken | null>;
+  invalidate(refresh_token: RefreshToken): Promise<void>;
+}
